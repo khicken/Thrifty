2f408d3f24685fb31f4223323b2c0d737551339c
kaleb <39206468+khicken@users.noreply.github.com>
2024-08-28 00:18:31 -0400

    pivot to selenium usage

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Thrifty
  Price estimator for items in a thrift store.
- Shoutout to Hope Nuevo Thrift in San Jose, CA!
+ Shoutout to Hope Nuevo Thrift in San Jose, CA.
+
+ Requirements before use:
+    - 64-bit installation of Windows
+    - Google Chrome* (v128)
+    - Webcam
--- scraper.py
+++ scraper.py
@@ -1,17 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import re, typing
 
-'''
-Scrapes a given url for a price in USD.
-'''
-def scraper(url):
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from amazoncaptcha import AmazonCaptcha
+from selenium.webdriver.common.by import By
+
+# create window
+service = Service('./chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.binary_location = './chrome-win64/chrome.exe'
+driver = webdriver.Chrome(service=service, options=options)
+
+# boot into google.com like a regular human
+driver.maximize_window()
+driver.get("https://www.google.com/")
+driver.implicitly_wait(0.5)
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+driver.quit()
+
+print(driver.title)
+
+
+
+print(driver.page_source)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+'''  Returns a float (price) given a url  '''
+def scraper(url: str) -> float:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.84 Safari/537.3'
     }
+    
+
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     print(soup)
-    price = soup.find('span', {'class': 'a-price-whole'}).text
+
+    class_names = ['a-price-whole', '']
+    preprice = soup.find('span', {'class': class_names}).text
+
+    try:
+        print(f'price obtained: {float(price)}')
+    except:
+        price = re.sub(r'[0-9.]', '', preprice)
+        if re.findall('£'): # Convert Pound to US Dollar
+            price *= 1.32
     return price
 
-print(scraper('https://www.amazon.com/Crayola-Crayons-Non-Toxic-Coloring-Supplies/dp/B017MIRU96'))
+# testing
+print(scraper('https://www.ebay.com/itm/144732458614'))
