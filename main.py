e407d6e0253911bc224d169063d00ecfb52a4f59
kaleb <39206468+khicken@users.noreply.github.com>
2024-08-31 00:09:16 -0400

    first working prototype!!!1!1!!!1
    YIPPPPPPPPEEEEEEEEEEE

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import time, cv2
+import scraper
 
-# GUI
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(858, 582)
-        self.graphicsView = QtWidgets.QGraphicsView(Dialog)
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 861, 591))
-        self.graphicsView.setObjectName("graphicsView")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(380, 10, 101, 41))
-        font = QtGui.QFont()
-        font.setFamily("Verdana")
-        font.setPointSize(30)
-        font.setBold(False)
-        font.setWeight(50)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
+# main file, opens camera and performs stuff
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+if __name__ == '__main__':
+    # open capture
+    cap = cv2.VideoCapture(0)
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "Thrifty"))
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        quit(1)
 
+    while True:
+        ret, frame = cap.read()
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    sys.exit(app.exec_())
+        if not ret:
+            print("Error: Could not read frame from webcam.")
+            quit(1)
+
+        cv2.imshow("Camera", frame)
+
+        key = cv2.waitKey(1)
+
+        if key == ord('s'):
+            cv2.imwrite("temp.png", frame)
+            print("Image saved as 'temp.png'")
+            time.sleep(2)
+            print(f'estimated price: {scraper.scraper():.2f}')
+        elif key == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            quit()
--- scraper.py
+++ scraper.py
@@ -3,76 +3,77 @@ import pyautogui
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager 
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from amazoncaptcha import AmazonCaptcha
+import numpy as np
 
-img_url = os.getcwd() + r'\samples\waterbottle2.png'
-
-# create window
-service = Service('./chromedriver.exe')
-options = webdriver.ChromeOptions()
-options.binary_location = './chrome-win64/chrome.exe'
-options.add_argument('--headless') # comment when testing
-browser = webdriver.Chrome(service=service, options=options)
-
-# boot into google.com like a regular human
-browser.maximize_window()
-browser.implicitly_wait(5)
-browser.get("https://www.google.com/")
-
-# make the google lens search
-browser.implicitly_wait(random.uniform(0.5, 2.0))
-browser.find_element(By.CLASS_NAME, 'nDcEnd').click() # click lens button
-browser.implicitly_wait(random.uniform(0.5, 2.0))
-browser.find_element(By.CLASS_NAME, 'DV7the').click() # click upload image button
-time.sleep(1)
-pyautogui.write(img_url) # upload image
-pyautogui.press('enter')
-browser.implicitly_wait(3)
-try:
-    # Wait up to 10 seconds for elements with class 'DdKZJb' to be present
-    elements = WebDriverWait(browser, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'oOZ3vf'))
-    )
-
-    # Iterate through each element and print its text
-    for e in elements:
-        print(f'found: {e.text}')
-
-except Exception as e:
-    print(f"An error occurred: {e}")
-    
-time.sleep(10000)
-
-browser.get("https://www.amazon.com/Stanley-IceFlow-Stainless-Steel-Tumbler/dp/B0CT4BB651")
-browser.find_element_by_class_name
-
-# terminate program
-browser.quit()
-
-'''  Returns a float (price) given a url  '''
-def scraper(url: str) -> float:
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.84 Safari/537.3'
-    }
-    
-
-    class_names = ['a-price-whole', '']
-    preprice = soup.find('span', {'class': class_names}).text
-
+def scraper() -> list:
     try:
-        print(f'price obtained: {float(price)}')
-    except:
-        price = re.sub(r'[0-9.]', '', preprice)
-        if re.findall('£'): # Convert Pound to US Dollar
-            price *= 1.32
-    return price
+        # create window
+        service = Service('./chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        options.binary_location = './chrome-win64/chrome.exe'
+        # options.add_argument('--headless') # need to fix this later, will have to remove pyautogui
+        browser = webdriver.Chrome(service=service, options=options)
 
-# testing
-print(scraper('https://www.ebay.com/itm/144732458614'))
+        # boot into google.com like a regular human
+        browser.maximize_window()
+        browser.implicitly_wait(5)
+        browser.get("https://www.google.com/")
+
+        # make the google lens search
+        browser.implicitly_wait(random.uniform(0.5, 2.0))
+        browser.find_element(By.CLASS_NAME, 'nDcEnd').click() # click lens button
+        browser.implicitly_wait(random.uniform(0.5, 2.0))
+        browser.find_element(By.CLASS_NAME, 'DV7the').click() # click upload image button
+        time.sleep(1)
+        pyautogui.write(os.getcwd() + r'\temp.png') # upload image
+        pyautogui.press('enter')
+
+        # scrape prices on listing page
+        time.sleep(5)
+        elements = browser.find_elements(By.CLASS_NAME, 'DdKZJb')
+
+        # time.sleep(60) # for custom price analysis
+
+        # convert price strings to prices
+        prices = []
+        for e in elements:
+            mul = 1.0 # multiplier for curr conversion
+            if re.findall('£', e.text): # convert Pound to US Dollar
+                mul = 1.32
+            chars = ''.join(re.findall(r'\d+\.\d+', e.text))
+            if not chars: # empty string
+                continue
+            price = float(''.join(chars)) * mul
+            print(price)
+            prices.append(price)
+
+        # old price estimates
+        print(f'median: {np.median(prices)} \n mean: {np.mean(prices)} \n 25th percentile: {np.percentile(prices, 25)} \n 10th percentile: {np.percentile(prices, 10)}')
+        print(f'min: {np.min(prices)} \n max: {np.max(prices)}')
+
+        # price filtering
+        q1, q3 = np.percentile(prices, 25), np.percentile(prices, 75)
+        iqr = 1.5 * (q3 - q1)
+        i = 0
+        while i < len(prices):
+            if prices[i] <= 0 or prices[i] >= iqr: # 
+                del prices[i]
+            else:
+                i += 1
+        
+        # new price estimates
+        print(f'median: {np.median(prices)} \n mean: {np.mean(prices)} \n 25th percentile: {np.percentile(prices, 25)} \n 10th percentile: {np.percentile(prices, 10)}')
+        print(f'min: {np.min(prices)} \n max: {np.max(prices)}')
+
+        return np.percentile(prices, 20) * 0.25
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        quit(1)
+    finally:
+        browser.quit()
